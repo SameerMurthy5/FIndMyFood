3647d270adf3e694234cc41b9d0f07fbf90cee7e
SameerMurthy5 <murthy25@purdue.edu>
2025-07-01 23:33:01 -0400

    fixed bugs for prod

--- lib/actions.ts
+++ lib/actions.ts
@@ -6,7 +6,7 @@ import { Provider } from '@supabase/supabase-js'
 const signInWith = (provider: Provider) => async () => {
   const supabase = await createClient()
 
-  const auth_callback_url = `${process.env.SITE_URL}/auth/callback?next=/protected`
+  const auth_callback_url = `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback?next=/protected`
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
--- types/session.ts
+++ types/session.ts
@@ -5,4 +5,3 @@ export interface SessionState {
     messages: { role: "user" | "assistant"; content: string }[];
     lastLLMResponse: LLMResponse | null;
 }
-  
